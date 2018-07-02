d2bab40b7f7ccdf13c211604bb2518416a760968
ivan <ivansu.webmail@gmail.com>
2018-07-03 03:10:56 +0700

    Upload exercises-4

---
+++ exercises-4.js
@@ -0,0 +1,59 @@
+var tanggal = 20;
+var bulan = 2;
+var tahun = 2000;
+
+var namaBulan;
+
+switch(bulan) {
+  case 1:
+    namaBulan = 'Januari';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 2:
+    namaBulan = 'Februari';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 3:
+    namaBulan = 'Maret';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 4:
+    namaBulan = 'April';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 5:
+    namaBulan = 'Mei';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 6:
+    namaBulan = 'Juni';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 7:
+    namaBulan = 'Juli';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 8:
+    namaBulan = 'Agustus';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 9:
+    namaBulan = 'September';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 10:
+    namaBulan = 'Oktober';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 11:
+    namaBulan = 'November';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  case 12:
+    namaBulan = 'Desember';
+    console.log(tanggal + ' ' + namaBulan + ' ' + tahun);
+    break;
+  default:
+    console.log('Periksa kembali Inputan bulan');
+    break;
+}
