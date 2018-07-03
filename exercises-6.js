dad3fbf99a16c18a75ae37d23c5cb5c464de206a
ivan <ivansu.webmail@gmail.com>
2018-07-03 17:33:02 +0700

    Upload exercises-6

---
+++ exercises-6.js
@@ -0,0 +1,42 @@
+// 1. Melakukan Looping Menggunakan While
+// var x = 2;
+//
+// console.log('LOOPING PERTAMA');
+// while(x <= 20) {
+//   console.log(x + ' - I love Coding');
+//   x = x + 2;
+// }
+
+// var y = 20;
+//
+// console.log('LOOPING KEDUA');
+// while(y >= 2) {
+//   console.log(y + ' - I will become FullStack Developer');
+//   y = y - 2;
+// }
+
+// 2. Melakukan Looping Menggunakan For
+// var i;
+//
+// console.log('LOOPING PERTAMA');
+// for(i = 1; i <= 20; i++) {
+//   console.log(i + ' - I love Coding');
+// }
+//
+// console.log('LOOPING KEDUA');
+// for(i = 20; i >= 1; i--) {
+//   console.log(i + ' - I will become FullStack Developer');
+// }
+
+// 3. Angka Ganjil dan Genap
+var a = 1;
+
+while(a <= 100) {
+  if((a % 2) !== 0) {
+    console.log('GANJIL');
+    a++;
+  } else {
+    console.log('GENAP');
+    a++;
+  }
+}
