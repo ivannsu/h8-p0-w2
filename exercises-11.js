9c1a89abf77d8ac148c277f6712d62965d07d4b2
ivan <ivansu.webmail@gmail.com>
2018-07-04 00:27:31 +0700

    Finish exercises-11

---
+++ exercises-11.js
@@ -0,0 +1,22 @@
+var result;
+var flag;
+
+function balikKata(kata) {
+  result = '';
+  flag = kata.length;
+
+  while(result.length < kata.length) {
+    var character = kata[flag - 1];
+    result += character;
+    flag--;
+  }
+
+  return result;
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
