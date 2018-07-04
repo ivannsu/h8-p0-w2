a07804e5dfe9f4db82d89145863a2d894f214c3b
ivan <ivansu.webmail@gmail.com>
2018-07-04 22:56:41 +0700

    Update exercises-7, LOOPING

---
+++ exercises-7.js
@@ -0,0 +1,17 @@
+// 1. Menyusun Barisan Bintang
+var rows1 = 5;
+var i;
+
+for(i = 0; i < rows1; i++) {
+  console.log('*');
+}
+
+// 2. Menyusun Barisan Bintang Dengan Nested Looping
+var asterisks = '*';
+var rows2 = 5;
+var x;
+var y;
+
+for(x = 0; x < rows2; x++) {
+  
+}
